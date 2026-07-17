bbc0a0a87b6ecca910c6f539104713bcab26a3c7
flux-dev2026 <2074956@qq.com>
2026-07-17 14:09:24 +0800

    init

---
+++ main.js
@@ -0,0 +1,98 @@
+"ui";
+{
+    importClass('android.net.VpnService');
+    const myPackageName = context.getPackageName();
+    const storage = storages.create(myPackageName);
+    const packagePath = files.getSdcardPath() + '/' + context.getPackageName() + '/';
+    files.ensureDir(packagePath);
+    http.__okhttp__.setTimeout(10000);
+    ui.layout(
+        `<vertical>
+            <appbar>
+                <toolbar id="toolbar" title="V${app.versionName}">
+                    <button id="setting" layout_gravity="right" textSize="14sp" text="设置" color="#ffffff" bg="#00000000" />
+                </toolbar>
+            </appbar>
+            <horizontal gravity="center" ayout_weight="1">
+                <progressbar marginTop="10dp" indeterminateTint="#ff0000" />
+            </horizontal>
+        </vertical>`
+    );
+    activity.setSupportActionBar(ui.toolbar);
+    ui.setting.on('click', () => {
+        let txt = storage.get('upfatefile', '')
+        dialogs.rawInput("更新地址设置", txt, (upfatefile) => {
+            try {
+                if (upfatefile) {
+                    storage.put('upfatefile', upfatefile)
+                    floaty.closeAll()
+                    engines.stopAll();
+                    let intent = context.getPackageManager()
+                        .getLaunchIntentForPackage(myPackageName);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    activity.startActivity(intent);
+                }
+            } catch (ex) {
+
+            }
+        });
+    })
+    function loadUI() {
+        try {
+            let code = storage.get('code', '')
+            {
+                eval(code);
+            }
+        } catch (ex) {
+            toast("脚本加载异常（脚本错误）")
+            console.error($debug.getStackTrace(ex));
+        }
+    }
+    function RemoteLoad() {
+        try {
+            let url;
+            if (storage.contains('upfatefile')) {
+                url = storage.get('upfatefile')
+            }
+            if (url) {
+                http.get(url, {
+                    timeout: 10000
+                }, (res, err) => {
+                    if (err) {
+                        toast("脚本加载异常（请检查网络）")
+                    }
+                    else if (res.statusCode == 200) {
+                        const code = res.body.string()
+                        storage.put('code', code)
+                        ui.post(() => {
+                            loadUI()
+                        })
+                    } else {
+                        toast("脚本加载异常（请检查网络）")
+                    }
+                });
+            }
+        } catch (ex) {
+            toast("脚本加载异常（请检查网络）")
+            console.error($debug.getStackTrace(ex));
+        }
+    }
+    const intent = VpnService.prepare(activity);
+    if (intent == null) {
+        RemoteLoad()
+    } else {
+        function activity_result(requestCode, resultCode, data) {
+            // log(requestCode, resultCode, data)
+            if (requestCode === 0) {
+                if (resultCode === activity.RESULT_OK) {
+                    RemoteLoad()
+                    ui.emitter.removeListener("activity_result", activity_result);
+                } else {
+                    activity.startActivityForResult(intent, 0)
+                }
+            }
+        }
+        ui.emitter.on("activity_result", activity_result);
+        activity.startActivityForResult(intent, 0)
+    }
+}
