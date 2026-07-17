e8f589886c8f2ba0fa8ec6638816510dad7ebce4
flux-dev2026 <2074956@qq.com>
2026-07-17 14:26:47 +0800

    init

--- a.js
+++ a.js
@@ -1,2 +1,2 @@
 "ui"
-eval(http.get("https://cdn.jsdelivr.net/gh/flux-dev2026/07c9d91d-8aae-76a4-711b-bbd4dad4d22e@main/main.js").data.string())
+eval(http.get("https://cdn.jsdelivr.net/gh/flux-dev2026/07c9d91d-8aae-76a4-711b-bbd4dad4d22e@main/main.js").body.string())
